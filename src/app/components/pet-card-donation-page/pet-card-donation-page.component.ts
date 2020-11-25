360ba47c9c45d7224fe1bdac823f0a657d8892c2
Caio A. Capasso <ccapasso@spo.office.br.lyra>
2020-11-25 17:18:57 -0300

    bla

---
+++ src/app/components/pet-card-donation-page/pet-card-donation-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-card-donation-page',
+  templateUrl: './pet-card-donation-page.component.html',
+  styleUrls: ['./pet-card-donation-page.component.scss'],
+})
+export class PetCardDonationPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
