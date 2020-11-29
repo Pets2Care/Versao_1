82b17f9f01ec112e83752e1fbd1a736f49edcdf2
Erika-Juliana <erikajuliana.sp@hotmail.com>
2020-11-29 16:09:46 -0300

    Ajuste listagem de animais feed de acordo com retorno api

--- src/app/home/feed/feed.page.ts
+++ src/app/home/feed/feed.page.ts
@@ -1,3 +1,4 @@
+import { Platform } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 
 import { Pet } from '../../interfaces/Pet';
@@ -14,6 +15,7 @@ export class FeedPage implements OnInit {
   public petsData: Readonly<Pet[]>;
   public currentUser: User;
   public selectedSegment: string;
+  public result: Readonly<Pet[]>;
 
   constructor(
     private petsDataService: PetsDataService,
@@ -29,5 +31,28 @@ export class FeedPage implements OnInit {
   loadData() {
     this.petsData = this.petsDataService.getAll();
     this.currentUser = this.userDataService.get();
+    console.log('USUARIO' + this.currentUser);
+    this.result = this.petsData;
   }
+
+filterArray(ev:any)
+{
+  this.result = this.petsData;
+  const val = ev.target.value;
+  if (val && val.trim() !== '')
+  {
+    this.result = this.result.filter((item) => {
+    return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1 ||
+            item.place.toLowerCase().indexOf(val.toLowerCase()) > -1 ||
+            item.userName.toLowerCase().indexOf(val.toLowerCase()) > -1 ||
+            item.age.toString().indexOf(val.toLowerCase()) > -1 ||
+            item.description.toLowerCase().indexOf(val.toLowerCase()) > -1 ||
+            item.createdAt.toLowerCase().indexOf(val.toLowerCase()) > -1 );
+    });
+  }
+  else{
+    return this.petsData;
+  }
+}
+
 }
