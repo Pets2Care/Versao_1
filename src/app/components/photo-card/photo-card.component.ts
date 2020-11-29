ad47748bc99ebd2037bec48e984c09fe7497d043
Erika-Juliana <erikajuliana.sp@hotmail.com>
2020-11-29 20:15:41 -0300

    Inclusao alerta de informacoes de adocao

--- src/app/components/photo-card/photo-card.component.ts
+++ src/app/components/photo-card/photo-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { UsersDataService } from '../../services/UsersDataService';
+import { UserDataService } from '../../services/UserDataService';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-photo-card',
   templateUrl: './photo-card.component.html',
@@ -7,6 +9,43 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PhotoCardComponent implements OnInit {
   @Input() public item;
-  constructor() {}
+  public donationUser;
+  public currentUser;
+  constructor(private usersDataService: UsersDataService,
+              private alertController: AlertController,
+              private userDataService: UserDataService)
+              {}
   ngOnInit() {}
+
+  async presentAlertDonation(contact: string) {
+    const alert = await this.alertController.create({
+      header: 'Oba!',
+      message: 'Para combinar os detalhes da adoção com o tutor temporário, por favor entre em contato através do e-mail: '  + contact,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+  
+  async presentAlertDetails() {
+    const alert = await this.alertController.create({
+      header: 'Atenção!',
+      message: 'Você disponibilizou esse animal para adoção.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  selectAnimal(donationUserId: number)
+  {
+    this.currentUser = this.userDataService.get();
+    this.donationUser = this.usersDataService.getById(donationUserId);
+    if (this.currentUser.id !== this.donationUser.id){
+      this.presentAlertDonation(this.donationUser.email);
+    }
+    else{
+      this.presentAlertDetails();
+    }
+  }
 }
